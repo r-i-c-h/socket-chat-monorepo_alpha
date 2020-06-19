1a6d86af5d32a9fc75e8a2fe7420744f8afe6aad
r-i-c-h <rich@richwerden.com>
2020-06-18 21:21:49 -0700

    triv:File Housekeeping

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Server is indeed alive');
+  res.send({ response: "I am alive" }).status(200);
 })
 
 module.exports = router;
