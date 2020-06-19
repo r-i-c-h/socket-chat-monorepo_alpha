def45adf1c5f3390ffe619500d3b951c3d70d3b1
r-i-c-h <rich@richwerden.com>
2020-06-18 22:49:05 -0700

    fix: status dot color wasn't working

--- client/src/components/Infobar/Infobar.js
+++ client/src/components/Infobar/Infobar.js
@@ -14,13 +14,13 @@ const CloseIcon = () => (
   </svg>
 )
 
-const StatusCircle = (status) => {
+const StatusCircle = (isConnected) => {
   return (
     <svg
       viewBox="0 0 10 10"
       xmlns="http://www.w3.org/2000/svg"
       className='status-dot'
-      fill={status ? "var(--clr-green)" : "var(--clr-red)"}
+      fill={isConnected === true ? "var(--clr-green)" : "var(--clr-red)"}
     >
       <circle cx="5" cy="5" r="5" />
     </svg>
@@ -30,7 +30,7 @@ const Infobar = ({ room, isConnected }) => {
   return (
     <header className="infoBar">
       <div className="leftInnerContainer">
-        <StatusCircle status={isConnected} />
+        <StatusCircle isConnected={isConnected} />
         <h3>Game Room: {room}</h3>
       </div>
       <div className="rightInnerContainer">
